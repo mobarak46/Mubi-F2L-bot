6bcd9f243b527071c534a8655e68c111079db80b
Mobarak <114818275+mobarak46@users.noreply.github.com>
2025-07-15 09:59:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -93,7 +93,7 @@ async def start():
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
-    await TechVJBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+    await TechVJBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format "Bot restarted on {} at {} by {}"
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
