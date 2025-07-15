3d88c783d3e22201f37c294f986d8c89acb175f4
Mobarak <114818275+mobarak46@users.noreply.github.com>
2025-07-15 07:00:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://smdfilterxbot.koyeb.app/")
 
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001654008278'))
