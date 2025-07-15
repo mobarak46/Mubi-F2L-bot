2e1950bcf53f78ac1e17534ec34b0560ed9b1f73
Mobarak <114818275+mobarak46@users.noreply.github.com>
2025-07-15 10:07:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,10 +63,14 @@ async def start():
     temp.U_NAME = me.username
     temp.B_NAME = me.first_name
     tz = pytz.timezone('Asia/Kolkata')
-    today = date.today()
-    now = datetime.now(tz)
-    time = now.strftime("%H:%M:%S %p")
-    await TechVJBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+today = date.today()
+now = datetime.now(tz)
+current_time = now.strftime("%H:%M:%S %p")
+
+await TechVJBot.send_message(
+    chat_id=LOG_CHANNEL,
+    text=script.RESTART_TXT.format(today, current_time, "Render Deploy")  # third arg
+)
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
